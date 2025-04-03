01d2d6fdb0bc209844a3cf232ca32ec12f3b0d0a
KevinJustinXavier <kevinjxavier@gmail.com>
2025-04-03 19:57:17 -0400

    LA2

---
+++ scripts/app.js
@@ -0,0 +1,29 @@
+const app = Vue.createApp({
+    data(){
+            myFullName = "Kevin Justin Xavier";
+
+            pressureMonitorValue = 65
+        return{
+            myFullName,
+            pressureMonitorValue,
+            pressureMonitorLevels: [
+                 { id: 'danger-low', color: 'danger', label: 'Danger - Low!', min: 0, max:10 },
+                 { id: 'warn-low', color: 'warning', label: 'Warning - Low', min: 11, max:30 },
+                 { id: 'safe', color: 'safe', label: 'Safe', min: 31, max: 70 },
+                 { id: 'warn-high', color: 'warning', label: 'Warning - High', min: 71,max: 90 },
+                 { id: 'danger-high', color: 'danger', label: 'Danger - High', min: 91,max: 100 }
+            ]
+        };
+    },
+     /// Define the methods for the app
+     methods: {
+        isActive(pressureLevel) {
+            /// Logic here for if the pressure is within the level parameters return boolean
+            return this.pressureMonitorValue>=pressureLevel.min&&this.pressureMonitorValue<=pressureLevel.max
+        }
+    }
+});
+
+/// Mount the app to the #app element
+app.mount('#app');
+
